36ca4581fd1cd56329877a38ef3897a737b389cc
dagasolo138-lgtm <dagasolo138@gmail.com>
2026-06-22 00:30:12 +0800

    fix: let Pages load CSS through the HTML entrypoint

--- src/main.js
+++ src/main.js
@@ -1,6 +1,3 @@
-import "./styles/tokens.css";
-import "./styles/app.css";
-import "./styles/sheet.css";
 import { createApp } from "./app/createApp.js";
 
 createApp(document.querySelector("#app"));
