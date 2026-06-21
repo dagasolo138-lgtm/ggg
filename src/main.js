b04fbc805d7f3f92674e6d3a1eb7bb037940ea98
dagasolo138-lgtm <dagasolo138@gmail.com>
2026-06-22 00:10:57 +0800

    refactor: add modular application entrypoint

---
+++ src/main.js
@@ -0,0 +1,6 @@
+import "./styles/tokens.css";
+import "./styles/app.css";
+import "./styles/sheet.css";
+import { createApp } from "./app/createApp.js";
+
+createApp(document.querySelector("#app"));
